e81cab0883456a96a26c01e973474ea07feba8d9
0h-n0 <kbu94982@gmail.com>
2018-07-03 18:46:55 +0900

    update:

--- frxpy/utils/mylogger.py
+++ frxpy/utils/mylogger.py
@@ -1,98 +1,30 @@
-from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL, NOTSET
-from logging import getLogger, StreamHandler, INFO, Formatter
+import logging
+import socket
+from logging import DEBUG
+from logging import Formatter
+from logging import getLogger
+from logging import StreamHandler
+import sys
 
-class MyLogger(object):
-    """
-    frxpy logger class.
+"""
 
-    :param str name: set name but thiss clas don't show name.
-    :param str space: set a charactor between messages.
-    :param str level: set initial log level
+.. note::
+    About Log Level
+     0  NOTEST
+    10  DEBUG
+    20  INFO
+    30  WARNING, WARN
+    40  ERROR
+    50  CRITICAL, FATAL
+"""
 
-    Examples:
+mylogger = getLogger(__name__)
+mylogger.setLevel(DEBUG)
+stream = sys.stderr
 
-    >>> m = MyLogger()
-    >>> logger = m.get_logger()
-    >>> logger.info('msg')
-
-    >>> m.add('msg1')
-    >>> m.add('msg2')
-    >>>
-    """
-    def __init__(self, name, output='', space=' ', level='INFO'):
-        self.name = __name__
-        self.logger = getLogger(name)
-        self.default_format = '%(asctime)20s  %(levelname)6s [%(name)20s]' \
-                              ' [L%(lineno)4d] [%(funcName)15s]:: %(message)20s'
-        formatter = Formatter(self.default_format)
-        self.handler = StreamHandler()
-        self.handler.setFormatter(formatter)
-        self.logger.addHandler(self.handler)        
-        self.set_level(level)
-            
-    def get_logger(self):
-        """
-        return logger.
-        """
-        return self.logger
-    
-    def push(self, msg):
-        """
-        Fisrt-In-Last-Out
-        """
-        pass
-
-    def _update_default_format(self):
-        """
-        updates default format. This method is only called from internal method.
-        """
-        default_format = self.default_format[:]
-        for imsg in self.msgs:
-            default_format += imsg
-        return default_format
-
-    def pop(self):
-        """
-        Fisrt-In-Last-Out
-        """
-        self.clean()
-        del self.msgs[0]
-        print('>>>>>', self.msgs)
-        formatter = Formatter(self._update_default_format())
-        self.handler.setFormatter(formatter)
-        self.logger.addHandler(self.handler)
-
-    def clean(self):
-        """
-        create new name and set new logger in this class.
-        """
-        self.logger.removeHandler(self.handler)
-        
-    def set_level(self, level):
-        """
-        set log level []
-        
-        Args:
-
-
-        :param str level: DEBUG
-        :param str level: INFO
-        :param str level: WARNING
-        :param str level: ERROR
-        :param str level: CRITICAL
-
-        """
-        
-        if level == 'DEBUG':
-            self.logger.setLevel(DEBUG)
-        elif level == 'INFO':
-            self.logger.setLevel(INFO)
-        elif level == 'WARNING':
-            self.logger.setLevel(WARNING)
-        elif level == 'ERROR':
-            self.logger.setLevel(ERROR)
-        elif level == 'CRITICAL':
-            self.logger.setLevel(CRITICAL)
-        else:
-            self.logger.setLevel(NOTSET)
-
+handler = StreamHandler(stream)
+fmt = '%(processName)s\t' \
+      '%(asctime)s\t%(module)-8s\tL%(lineno)-3s'\
+      '\t%(levelname)-7s\t%(message)s'
+handler.setFormatter(Formatter(fmt=fmt, datefmt='%Y/%m/%d %H:%M:%S'))
+mylogger.addHandler(handler)
